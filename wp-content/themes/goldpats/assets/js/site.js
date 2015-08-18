1424a8850266671f358bf9029ee0e91475c1ae1d
Andrew McNaughton <andrew@andymc.com.au>
2015-08-18 15:03:03 +0200

    Initial setup. Wordpress, Base, ACF

---
+++ wp-content/themes/goldpats/assets/js/site.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($) {
+
+	var Theme = {
+		init: function() {
+
+            // this.something();
+
+		}
+
+        // something: function() {
+
+        // }
+
+	}
+
+	Theme.init();
+
+	window.Theme = Theme;
+
+});
